ca769ef6c549d83baa3b181a01a5946437ff14fb
pepe6bf <pepe6bf@gmail.com>
2022-08-21 10:30:12 +0900

    Project Init

---
+++ spadeworker/src/main/java/com/study/spadeworker/SpadeworkerApplication.java
@@ -0,0 +1,13 @@
+package com.study.spadeworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpadeworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpadeworkerApplication.class, args);
+	}
+
+}
---
+++ spadeworker/src/test/java/com/study/spadeworker/SpadeworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spadeworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpadeworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
