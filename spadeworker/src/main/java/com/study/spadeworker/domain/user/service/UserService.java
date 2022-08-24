f65b5a3a0892d9d622d8d3852a20e3cb86bab50d
pepe6bf <pepe6bf@gmail.com>
2022-08-25 04:17:27 +0900

    feat: #5 현재 로그인 된 사용자 Entity 조회 기능 구현
    현재 로그인 된 사용자 Entity를 조회하는 기능을 구현함

---
+++ spadeworker/src/main/java/com/study/spadeworker/domain/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.study.spadeworker.domain.user.service;
+
+import com.study.spadeworker.domain.user.entity.User;
+import com.study.spadeworker.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByLoginId(authentication.getName());
+    }
+}
