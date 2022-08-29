ad2761fc899e72ef6fe7db0b8a2c440724724716
pepe6bf <pepe6bf@gmail.com>
2022-08-30 02:23:16 +0900

    fix: #21 인증 토큰 재발급 오류 해결

--- spadeworker/src/main/java/com/study/spadeworker/domain/auth/controller/AuthController.java
+++ spadeworker/src/main/java/com/study/spadeworker/domain/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.study.spadeworker.global.response.SingleResult;
 import com.study.spadeworker.global.util.CookieUtil;
 import com.study.spadeworker.global.util.HeaderUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
