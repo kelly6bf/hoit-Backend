9f5b74eb8d830b16b816fc8c618cf3dc4a2df523
pepe6bf <pepe6bf@gmail.com>
2022-09-06 04:43:33 +0900

    feat: UserService 사용자 entity 조회 로직 추가

--- spadeworker/src/main/java/com/study/spadeworker/domain/user/service/UserService.java
+++ spadeworker/src/main/java/com/study/spadeworker/domain/user/service/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -21,5 +23,10 @@ public class UserService {
 
     public UserAccountDto getUserAccountDto(User user) {
         return (user != null) ? UserAccountDto.from(user) : null;
+    }
+
+    public User getUserEntity(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 사용자가 존재하지 않습니다."));
     }
 }
