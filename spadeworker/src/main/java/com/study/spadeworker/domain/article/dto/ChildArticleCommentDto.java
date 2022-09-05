8f55abd7400ee100e363533549c7d024b27bed55
pepe6bf <pepe6bf@gmail.com>
2022-09-05 23:49:05 +0900

    feat: #20 ChildArticleCommentDto 추가
    대댓글 리스트를 응답하기 위한 DTO 추가

---
+++ spadeworker/src/main/java/com/study/spadeworker/domain/article/dto/ChildArticleCommentDto.java
@@ -0,0 +1,57 @@
+package com.study.spadeworker.domain.article.dto;
+
+import com.study.spadeworker.domain.article.entity.ArticleComment;
+import com.study.spadeworker.domain.user.dto.UserAccountDto;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ChildArticleCommentDto {
+
+    private Long id;
+    private Long parentCommentId;
+    private UserAccountDto recipient;
+    private String content;
+    private int likesCount;
+    private int dislikesCount;
+    private LocalDateTime createdAt;
+    private UserAccountDto user;
+
+    private ChildArticleCommentDto(
+                    Long id,
+                    Long parentCommentId,
+                    UserAccountDto recipient,
+                    String content,
+                    int likesCount,
+                    int dislikesCount,
+                    LocalDateTime createdAt,
+                    UserAccountDto userAccountDto
+    ) {
+        this.id = id;
+        this.parentCommentId = parentCommentId;
+        this.recipient = recipient;
+        this.content = content;
+        this.likesCount = likesCount;
+        this.dislikesCount = dislikesCount;
+        this.createdAt = createdAt;
+        this.user = userAccountDto;
+    }
+
+    public static ChildArticleCommentDto from(
+            ArticleComment articleComment,
+            UserAccountDto recipient,
+            UserAccountDto userAccountDto
+    ) {
+        return new ChildArticleCommentDto(
+                articleComment.getId(),
+                articleComment.getParentComment().getId(),
+                recipient,
+                articleComment.getContent(),
+                articleComment.getLikesCount(),
+                articleComment.getDislikesCount(),
+                articleComment.getCreatedAt(),
+                userAccountDto
+        );
+    }
+}
