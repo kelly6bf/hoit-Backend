065a5814da84a793a1fc890e349a1278573d828b
pepe6bf <pepe6bf@gmail.com>
2022-09-14 14:54:28 +0900

    refactor: #31 게시글 댓글 생성/수정 DTO 리펙토링
    게시글 댓글 생성/수정 DTO를 다음과 같이 리펙토링 하였다.
    * Bean Validation 수정
      * NotNull -> NotBlank
      * Size -> Length
    * Response 내부 객체의 불필요한 Builder 어노테이션 제거 및 생성자로 대체

--- spadeworker/src/main/java/com/study/spadeworker/domain/article/dto/articleComment/CreateArticleChildCommentDto.java
+++ spadeworker/src/main/java/com/study/spadeworker/domain/article/dto/articleComment/CreateArticleChildCommentDto.java
@@ -1,8 +1,9 @@
 package com.study.spadeworker.domain.article.dto.articleComment;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateArticleChildCommentDto {
@@ -11,9 +12,10 @@ public class CreateArticleChildCommentDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Request {
-        @NotNull(message = "게시글 PK는 필수 입니다.")
+        @NotBlank(message = "게시글 id는 필수입니다.")
         private Long articleId;
-        @NotNull(message = "댓글 본문은 필수 입니다.")
+        @NotBlank(message = "댓글 본문은 필수입니다.")
+        @Length(min = 1, max = 10000, message = "댓글 본문 길이 제한은 1이상 10000이하 입니다.")
         private String content;
         @Setter
         private Long parentCommentId;
@@ -23,14 +25,11 @@ public class CreateArticleChildCommentDto {
 
     @Getter
     @AllArgsConstructor
-    @Builder
     public static class Response {
         private Long articleCommentId;
 
-        public static Response of(@NonNull Long articleCommentId) {
-            return Response.builder()
-                    .articleCommentId(articleCommentId)
-                    .build();
+        public static Response of(@NonNull final Long articleCommentId) {
+            return new Response(articleCommentId);
         }
     }
 }
