89e3e80a590c95dcbe7ee41aa2500510fa29c4b3
pepe6bf <pepe6bf@gmail.com>
2022-09-15 15:09:04 +0900

    fix: CreateArticleChildCommentDto.Request 잘못된 Validation 어노테이션 수정
    기존에 사용한 @NotBlank에 경우 문자열 타입만 검증할 수 있기 때문에 "UnexpectedTypeException" 에러가 발생하였다. 현재 검증하고 싶은 필드 타입이 Long 이므로, @NotNull 로 변경해주었다.

--- spadeworker/src/main/java/com/study/spadeworker/domain/article/dto/articleComment/CreateArticleChildCommentDto.java
+++ spadeworker/src/main/java/com/study/spadeworker/domain/article/dto/articleComment/CreateArticleChildCommentDto.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateArticleChildCommentDto {
@@ -12,7 +13,7 @@ public class CreateArticleChildCommentDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Request {
-        @NotBlank(message = "게시글 id는 필수입니다.")
+        @NotNull(message = "게시글 id는 필수입니다.")
         private Long articleId;
         @NotBlank(message = "댓글 본문은 필수입니다.")
         @Length(min = 1, max = 10000, message = "댓글 본문 길이 제한은 1이상 10000이하 입니다.")
