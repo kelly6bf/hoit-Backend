3a64725761d0ab3e688037b025500d9548312a3f
pepe6bf <pepe6bf@gmail.com>
2022-09-17 16:51:10 +0900

    feat: #31 게시글 카테고리 검증을 위한 existsByTitle 쿼리메서드 추가
    특정 카테고리가 테이블 상에 존재하는 카테고리인지 검증하는 existsByTitle(String title) 쿼리 메서드를 추가하였다.

--- spadeworker/src/main/java/com/study/spadeworker/domain/article/repository/ArticleCategoryRepository.java
+++ spadeworker/src/main/java/com/study/spadeworker/domain/article/repository/ArticleCategoryRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ArticleCategoryRepository extends JpaRepository<ArticleCategory, Long> {
 
     ArticleCategory findByTitle(String category);
+
+    boolean existsByTitle(String title);
 }
