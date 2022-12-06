9c3c72396b96a7b1006dd5d35506d764bad1e073
pepe6bf <pepe6bf@gmail.com>
2022-12-06 18:21:43 +0900

    refactor: 기타 변경 사항 추가

--- spadeworker/src/main/java/com/study/spadeworker/domain/auth/controller/AuthController.java
+++ spadeworker/src/main/java/com/study/spadeworker/domain/auth/controller/AuthController.java
@@ -10,6 +10,7 @@ import com.study.spadeworker.global.util.HeaderUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
